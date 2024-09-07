a8b9f1a5d578e9d5087cd2c731e83dde3c42cdc8
Martz94 <130812797+Martz94@users.noreply.github.com>
2024-09-06 20:56:30 -0600

    cambios de contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,8 @@ import { BsGithub } from "react-icons/bs";
 import { AiTwotoneMail } from "react-icons/ai";
 import { BsTelephoneForwardFill } from "react-icons/bs";
 import { IoRemoveOutline } from "react-icons/io5";
+import { SiWhatsapp } from "react-icons/si";
+
 
 const Contact = () => {
     return (
@@ -33,13 +35,13 @@ const Contact = () => {
                         <p className=' text-base md:text-lg text-zinc-700 dark:text-zinc-400 mb-2'>Ciudad de México</p>
                         <p className=' text-base md:text-lg text-zinc-700 dark:text-zinc-400 mb-2'>Desarrollador Web</p>
                         <div className=' lg:mt-10 mb-0'>
-                            <p className=' text-base md:text-lg text-zinc-700 dark:text-zinc-400 mb-5'>Redes Sociales</p>
-                            <a href="https://www.linkedin.com/in/uriel-martinez-pascual-41ab2a116/" className=' text-sky-700 dark:text-sky-700 flex gap-3 my-7 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'> <BsLinkedin className=" w-8 h-8" />LINKEDIN</a>
-                            <a href="https://github.com/Martz94" className=' text-sky-700 dark:text-sky-700 flex gap-3 my-7 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'><BsGithub className=" w-8 h-8" />GITHUB</a>
-                            <a href="mailto:urielmartzpascual@gmail.com?Subject=Hola,%20Estoy%20interesado(a)%20en%20tu%20perfil" className=' text-sky-700 dark:text-sky-700  flex gap-3 my-7 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'><AiTwotoneMail className=" w-8 h-8" /> E-MAIL</a>
-                            <a href="tel:9211237512" className=" flex items-center text-sky-700 mt-7 hover:text-zinc-700 dark:hover:text-zinc-300 pe-2 gap-3 cursor-pointer text-sm lg:text-base">
-                                <BsTelephoneForwardFill className=' h-7 w-7' />CONTACTAR
+                            <a href="https://www.linkedin.com/in/uriel-martinez-pascual-41ab2a116/" className=' text-sky-700 dark:text-sky-700 flex gap-3 my-6 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'> <BsLinkedin className=" w-8 h-8" />LINKEDIN</a>
+                            <a href="https://github.com/Martz94" className=' text-sky-700 dark:text-sky-700 flex gap-3 my-6 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'><BsGithub className=" w-8 h-8" />GITHUB</a>
+                            <a href="mailto:urielmartzpascual@gmail.com?Subject=Hola,%20Estoy%20interesado(a)%20en%20tu%20perfil" className=' text-sky-700 dark:text-sky-700  flex gap-3 my-6 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'><AiTwotoneMail className=" w-8 h-8" /> E-MAIL</a>
+                            <a href="tel:9211237512" className=" flex items-center text-sky-700 my-6 hover:text-zinc-700 dark:hover:text-zinc-300 pe-2 gap-3 cursor-pointer text-sm lg:text-base">
+                                <BsTelephoneForwardFill className=' h-7 w-7' />TELEFONO
                             </a>
+                            <a href="https://wa.me/9211237512" className=' text-sky-700 dark:text-sky-700 flex gap-3 my-6 items-center text-sm lg:text-base hover:text-zinc-700 dark:hover:text-zinc-300'><SiWhatsapp  className=' h-8 w-8'/>WHATSAPP</a>
                         </div>
                     </div>
                 </div>
